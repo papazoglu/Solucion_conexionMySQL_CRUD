41fd44bcabaedd59e8b26814cb3f68724beeb915
Luis Papazoglu <lpapazoglu@gmail.com>
2025-09-18 09:42:11 -0300

    Agregar archivos de proyecto.

---
+++ Pj_ConexionMySQL/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Pj_ConexionMySQL
+{
+    partial class frmAlumnos
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            txtBuscar = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(51, 81);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(696, 263);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(560, 10);
+            button1.Name = "button1";
+            button1.Size = new Size(93, 47);
+            button1.TabIndex = 3;
+            button1.Text = "Mostrar datos DataReader";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // txtBuscar
+            // 
+            txtBuscar.Enabled = false;
+            txtBuscar.Location = new Point(51, 34);
+            txtBuscar.Name = "txtBuscar";
+            txtBuscar.Size = new Size(203, 23);
+            txtBuscar.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(260, 29);
+            button2.Name = "button2";
+            button2.Size = new Size(52, 30);
+            button2.TabIndex = 5;
+            button2.Text = "Buscar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(512, 370);
+            button3.Name = "button3";
+            button3.Size = new Size(105, 32);
+            button3.TabIndex = 6;
+            button3.Text = "modificar";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(377, 368);
+            button4.Name = "button4";
+            button4.Size = new Size(115, 34);
+            button4.TabIndex = 7;
+            button4.Text = "Nuevo";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(634, 370);
+            button5.Name = "button5";
+            button5.Size = new Size(113, 33);
+            button5.TabIndex = 8;
+            button5.Text = "Eliminar";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // frmAlumnos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(808, 431);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(txtBuscar);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "frmAlumnos";
+            Text = "Form1";
+            Load += frmAlumnosLista_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private TextBox txtBuscar;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+    }
+}
---
+++ Pj_ConexionMySQL/Form1.cs
@@ -0,0 +1,186 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Pj_ConexionMySQL
+{
+
+    public partial class frmAlumnos : Form
+    {
+        //variables de clase (globales)
+        public MySqlConnection conexion;
+        DataTable tabla;
+       
+
+        public frmAlumnos()
+        {
+            InitializeComponent();
+            String cadenaConexion = "server=localhost;user=root;password=root;database=lenguaje1";
+            conexion = new MySqlConnection(cadenaConexion);
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnMostrarDatos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String query = "SELECT * FROM alumnos";
+                MySqlCommand comando = new MySqlCommand(query, conexion);
+                MySqlDataAdapter adaptador = new MySqlDataAdapter(comando);
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+                dataGridView1.DataSource = tabla;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en la consulta: " + ex.Message);
+                return;
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String query = "select * from alumnos";
+                MySqlCommand comando = new MySqlCommand(query, conexion);
+                MySqlDataReader reader = comando.ExecuteReader();
+                tabla = new DataTable();
+                tabla.Load(reader);
+                dataGridView1.DataSource = tabla;
+                //while (reader.Read())
+                //{
+                //    Console.WriteLine(reader["nombre"].ToString());
+                //    MessageBox.Show(reader["nombre"].ToString());   
+                //}
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show("Revise el usuario y contraseña de la base de datos");
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataView vista = new DataView(tabla);
+            //vista.RowFilter = "nombre LIKE '%" + this.textBox1.Text + "%'";
+            if (string.IsNullOrEmpty(this.txtBuscar.Text))
+                vista.RowFilter = "";
+            else
+            {
+                vista.RowFilter = "id = " + this.txtBuscar.Text;
+            }
+            
+            //vista.RowFilter = "Convert(id, 'System.String') LIKE '%" + this.textBox1.Text + "%'";
+
+            if (vista.Count > 0)
+            {
+                String id = vista[0]["id"].ToString();
+                String nombre = vista[0]["nombre"].ToString() ?? "";
+                int dni = Convert.ToInt32(vista[0]["dni"]);
+                MessageBox.Show($"Id: {id} - Nombre: {nombre} - DNI: {dni}");
+            }
+
+            dataGridView1.DataSource = vista;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.dataGridView1.CurrentRow != null)
+                {
+                    String nombre = this.dataGridView1.CurrentRow.Cells["nombre"].Value?.ToString() ?? "";
+                    String dni = this.dataGridView1.CurrentRow.Cells["dni"].Value?.ToString() ?? "";
+                    int id = Convert.ToInt32(this.dataGridView1.CurrentRow.Cells["id"].Value);
+                    MessageBox.Show($"nombre seleccionado: {nombre}");
+
+                    Form alumno = new frmAlumnosForm(conexion, id, nombre, dni,2);
+                    alumno.Show();
+                }
+                else
+                {
+                    MessageBox.Show("no hay filas");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //String nombre = this.dataGridView1.CurrentRow.Cells["nombre"].Value?.ToString() ?? "";
+            //String dni = this.dataGridView1.CurrentRow.Cells["dni"].Value?.ToString() ?? "";
+            //int id = Convert.ToInt32(this.dataGridView1.CurrentRow.Cells["id"].Value);
+            //MessageBox.Show($"nombre seleccionado: {nombre}");
+
+            Form alumno = new frmAlumnosForm(conexion, 1,this);
+            alumno.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string id = this.dataGridView1.CurrentRow.Cells["id"].Value.ToString();
+            string query = "delete from alumnos where id=" + id;
+            MySqlCommand comando = new MySqlCommand(query, conexion);
+            int resul = comando.ExecuteNonQuery();
+            MessageBox.Show($"La consulta devolvio: {resul}");
+            obtenerTodos();
+        }
+
+        private void frmAlumnosLista_Load(object sender, EventArgs e)
+        {
+         
+            try
+            {
+                conexion.Open();
+                this.txtBuscar.Enabled = true;
+                obtenerTodos();
+                //MessageBox.Show("Conexión exitosa");
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error de conexión: " + ex.Message);
+            }
+        }
+
+        public void obtenerTodos()
+        {
+            try
+            {
+                String query = "select * from alumnos";
+                MySqlCommand comando = new MySqlCommand(query, conexion);
+                MySqlDataReader reader = comando.ExecuteReader();
+                tabla = new DataTable();
+                tabla.Load(reader);
+                dataGridView1.DataSource = tabla;
+                //while (reader.Read())
+                //{
+                //    Console.WriteLine(reader["nombre"].ToString());
+                //    MessageBox.Show(reader["nombre"].ToString());   
+                //}
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show("Revise el usuario y contraseña de la base de datos");
+            }
+        }
+    }
+}
